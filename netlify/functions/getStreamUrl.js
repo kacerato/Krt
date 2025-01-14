bdd2a4fb8899f399ec8405ad6f7859d04c0cdad5
kacerato <79872194+kacerato@users.noreply.github.com>
2025-01-14 19:07:00 -0300

    Atualizar o getStreamUrl.js

--- netlify/functions/getStreamUrl.js
+++ netlify/functions/getStreamUrl.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
     const { vodId } = JSON.parse(event.body);
     const accessToken = process.env.TWITCH_ACCESS_TOKEN;
     const clientId = process.env.TWITCH_CLIENT_ID;
-
+ 
     // Obter o token de acesso para o VOD
     const tokenResponse = await fetch(`https://api.twitch.tv/helix/videos?id=${vodId}`, {
       headers: {
