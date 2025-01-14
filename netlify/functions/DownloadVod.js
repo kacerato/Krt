ba46276440df19445d08063e6770ef3052792525
kacerato <79872194+kacerato@users.noreply.github.com>
2025-01-14 19:07:11 -0300

    Atualizar o DownloadVod.js

--- netlify/functions/DownloadVod.js
+++ netlify/functions/DownloadVod.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
     const { vodId } = JSON.parse(event.body);
     const accessToken = process.env.TWITCH_ACCESS_TOKEN;
     const clientId = process.env.TWITCH_CLIENT_ID;
-
+ 
     // Obter informações do VOD
     const vodInfoResponse = await fetch(`https://api.twitch.tv/helix/videos?id=${vodId}`, {
       headers: {
